6acb0fb44217746e608ccbb2fde507e08dc9afbb
wt <wangtao@cplh.com.cn>
2015-02-02 22:06:48 +0800

    added the excel app

--- app/config/app.php
+++ app/config/app.php
@@ -123,6 +123,7 @@ return array(
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
 		'Jacopo\Authentication\AuthenticationServiceProvider',
+		'Maatwebsite\Excel\ExcelServiceProvider',
 
 	),
 
