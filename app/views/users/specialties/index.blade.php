a369ab45dcda54a3ea0e78d54177f04cdc19526b
wt <wangtao@cplh.com.cn>
2015-03-27 19:42:15 +0800

    change the matches with colleges list

--- app/views/users/specialties/index.blade.php
+++ app/views/users/specialties/index.blade.php
@@ -16,7 +16,7 @@
                     <ul class="nav" id="side-menu">
         <li><a href="{{URL::to('users')}}" class="btn btn-default btn1" type="button">我的测评</a></li>
         <li><a href="{{URL::to('users/specialties')}}" class="btn btn-default btn1" type="button">专业列表</a></li>
-        <li><a href="{{URL::to('users/matches')}}" class="btn btn-default btn1" type="button">志愿匹配</a></li>
+        <li><a href="{{URL::to('users/matches')}}" class="btn btn-default btn1" type="button">院校列表</a></li>
 	       <li>
                 <a href="{{URL::to('users/collects')}}" class="btn btn-default btn1" type="button">我的收藏</a>
                <ul class="nav nav-second-level">
@@ -27,7 +27,7 @@
                                     <a href="{{URL::to('users/collects/specialites')}}">专业收藏</a>
                                 </li>
                                 <li>
-                                   <a href="{{URL::to('users/collects/training')}}" >课程收藏</a>
+                                   <a>课程收藏</a>
                                 </li>
                                 <li>
                                    <a href="{{URL::to('users/collects/others')}}" >其他收藏</a>
