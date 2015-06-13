087bba23c2a5b627831d5fc102efe7bd3240e64d
wt <wangtao@cplh.com.cn>
2015-06-14 07:42:22 +0800

    added 7 pages for career

---
+++ app/controllers/users/CareerController.php
@@ -0,0 +1,97 @@
+<?php
+namespace App\Controllers\Users;
+ 
+use Area,City,College,Specialty,Province,UserProfile,ProfileField,Zylb,Ktest,Kresult,Flzhuanye,Student,Career,Collect;
+use Input, Notification, Redirect, Sentry, Str;
+
+use App\Services\Validators\PageValidator;
+use App\Services\Ktest\Cryptographer;
+use App\Services\Ktest\HesClient;
+use App\Services\Ktest\Kclasses;
+use App\Services\Sclass\JsonArrayHandle;
+use SoapBox\Formatter\Formatter;
+
+class CareerController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 * GET /users/career
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 * GET /users/career/create
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 * POST /users/career
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		//
+	}
+
+	/**
+	 * Display the specified resource.
+	 * GET /users/career/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 * GET /users/career/{id}/edit
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 * PUT /users/career/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 * DELETE /users/career/{id}
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
