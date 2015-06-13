7ba60e550d19084d8ae31533c4768cdcc9d1ddab
wt <wangtao@cplh.com.cn>
2015-06-13 20:05:19 +0800

    added video,career,collect  model and tables

--- app/models/Collect.php
+++ app/models/Collect.php
@@ -2,4 +2,14 @@
 
 class Collect extends \Eloquent {
 	protected $fillable = [];
+	protected $table = 'collect';  
+	public function user(){
+    return $this->belongsTo('User','user_id');
+    }
+	public function college(){
+    return $this->belongsTo('College','co_id');
+    }
+	public function career(){
+    return $this->belongsTo('Career','career_id');
+	}
 }
--- app/views/users/collects/index.blade.php
+++ app/views/users/collects/index.blade.php
@@ -1,61 +1,28 @@
 @extends('master')
-@section('hdsrc')
-<link href={{ URL::asset('images/css/sb-admin-2.css') }} rel="stylesheet">
-<link href={{ URL::asset('images/css/timeline.css') }} rel="stylesheet">
-<script type="text/javascript" src={{ URL::asset('images/metisMenu/dist/metisMenu.js') }}></script>
-<script type="text/javascript" src={{ URL::asset('images/js/sb-admin-2.js') }}></script>
-
-@stop
 @section('header')
 @stop
 @section('content')
 {{ Notification::showAll() }}
 	<div class='col-md-2 text-center  slidbar_bg'>
-		<div class="sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-        <li><a href="{{URL::to('users')}}" class="btn btn-default btn1" type="button">我的测评</a></li>
-        <li><a href="{{URL::to('users/specialties')}}" class="btn btn-default btn1" type="button">专业列表</a></li>
-        <li><a href="{{URL::to('users/matches')}}" class="btn btn-default btn1" type="button">院校列表</a></li>
-	       <li>
-                <a href="{{URL::to('users/collects')}}" class="btn btn-default btn1" type="button">我的收藏</a>
-               <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="{{URL::to('users/collects/colleges')}}" >院校收藏</a>
-                                </li>
-                                <li>
-                                    <a href="{{URL::to('users/collects/specialites')}}">专业收藏</a>
-                                </li>
-                                <li>
-                                 <a课程收藏</a>
-                                </li>
-                                <li>
-                                   <a href="{{URL::to('users/collects/others')}}" >其他收藏</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-      </ul>
-	</div>
-	</div>
+		
+       @include('users.slidbar')
+        
+
 	</div>
 	<div class='col-md-7'>
 		<h2>
 			会员中心
 		</h2>
-		<h2>
-			会员名称：
-		</h2>
-		<p>会员信息：</p>
-		
-		 
-	<ul>
-		<li><a href="{{URL::to('users/collects/colleges')}}" >院校收藏</a>收藏您所关注的院校，包括院校的批次科类等信息</li>
-        <li><a href="{{URL::to('users/collects/specialites')}}">专业收藏</a>收藏您所关注的专业，以便于您对这些专业 进行对比与匹配</li>
-        <li><a href="{{URL::to('users/collects/training')}}" >课程收藏</a>收藏您所喜好的课程，您可以随时通过这些课程进行学习</li>
-        <li><a href="{{URL::to('users/collects/others')}}" >其他收藏</a>对于您的测试结果和一些视频信息，可以在此处进行收藏并随时查阅</li>
-       
-	</ul>
+		<h3>
+			我的收藏
+		</h3>
+		 <p>
+	<div class="btn-group">
+  <button class="btn btn-large btn-primary" type="button">职业收藏</button>
+  <button class="btn btn-large" type="button">院校收藏</button>
+ </div>
+</p>
+
             </div>
 <div class='col-md-3'>
 		@include('ads')
